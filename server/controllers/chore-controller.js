ec2838556c27be28cb1620c95fcd4b680e93a214
Cory Thompson <cory.thompson98@gmail.com>
2022-04-14 11:21:25 -0500

    getAllChores controller added

--- server/controllers/chore-controller.js
+++ server/controllers/chore-controller.js
@@ -2,11 +2,22 @@ const { Chore, User } = require('../models');
 
 const choreController = {
     getAllChores(req, res) {
-
+        Chore.find({})
+        .populate({
+            path: 'user',
+            select: '-__v'
+        })
+        .select('-__v')
+        .sort({ _id: -1 })
+        .then(dbChoreData => res.json(dbChoreData))
+        .catch(err => {
+            console.log(err);
+            res.status(400).json(err)
+        });
     },
 
     getChoreById({ params }, res) {
-
+        
     },
 
     createChore({ params, body }, res) {
@@ -18,7 +29,7 @@ const choreController = {
     },
 
     deleteChore({ params, body}, res) {
-        
+
     }
 };
 
