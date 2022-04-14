d4375f967a8014a02213e2986a160111ca2a7b54
Cory Thompson <cory.thompson98@gmail.com>
2022-04-14 11:18:04 -0500

    beginnings of the chore-controller under the controllers directory

---
+++ server/controllers/chore-controller.js
@@ -0,0 +1,25 @@
+const { Chore, User } = require('../models');
+
+const choreController = {
+    getAllChores(req, res) {
+
+    },
+
+    getChoreById({ params }, res) {
+
+    },
+
+    createChore({ params, body }, res) {
+
+    },
+
+    updateChore({ params, body }, res) {
+
+    },
+
+    deleteChore({ params, body}, res) {
+        
+    }
+};
+
+module.exports = choreController;
