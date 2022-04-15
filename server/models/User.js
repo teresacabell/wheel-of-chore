829799b30a803c2c84d143df4d9bb31ef322e7ae
teresacabell <teresacabll@gmail.com>
2022-04-14 20:45:11 -0500

    added chore routes and user routes

--- server/models/User.js
+++ server/models/User.js
@@ -1,47 +1,50 @@
-const { Schema, model } = require('mongoose');
-const bcrypt = require('bcrypt');
+const { Schema, model } = require("mongoose");
 
+const UserSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+    },
+    color: {
+      type: String,
+      allowNull: false,
+      defaultValue: "#ff9acd",
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [/.+@.+\..+/],
+    },
+    password: {
+      type: String,
+      allowNull: false,
+    },
+    chores: [
+      {
+        type: Schema.Types.ObjecId,
+        ref: "Chore",
+      },
+    ],
+    members: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+    id: false,
+  }
+);
 
-// const User = model('User', UserSchema);
-
-// const UserSchema = new Schema({
-//     username: {
-//         type: String,
-//         unique: true,
-//         required: true,
-//         trim: true
-//     },
-//     color: {
-//         type: String,
-//         allowNull: false,
-//         defaultValue: "#ff9acd"
-//     },
-//     email: {
-//         type: String,
-//         required: true,
-//         unique: true,
-//         match: [/.+@.+\..+/]
-//     },
-//     password: {
-//         type: String,
-//         allowNull: false
-//     }
-// });
-// User.associate = function (models) {
-//     User.belongsTo(models.Household, {
-//         foreignKey: { allowNull: false }
-//     }
-//     )};
-//     User.hasMany(models.Chore);
-
-//     // how to use bcrypt to make a password hash and verify password
-//     User.addHook.beforeCreate(user) {
-//         newUser.password = brcypt.hash(newUser.password, 10);
-
-//         return newUser;
-//     }
-
-
-
+const User = model("User", UserSchema);
 
 module.exports = User;
--- server/routes/api/user-routes.js
+++ server/routes/api/user-routes.js
@@ -1,22 +1,19 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
 const {
-    getUsers,
-    addUser,
-    getSingleUser,
-    updateUser,
-    deleteUser
-} = require('../../controllers/user-controller');
+  getUsers,
+  addUser,
+  getSingleUser,
+  updateUser,
+  deleteUser,
+  addMember,
+  deleteMember,
+} = require("../../controllers/user-controller");
 
-router
-    .route('/')
-    .get(getUsers)
-    .post(addUser);
+router.route("/").get(getUsers).post(addUser);
 
-router
-    .route('/:id')
-    .get(getSingleUser)
-    .put(updateUser)
-    .delete(deleteUser)
+router.route("/:userId").get(getSingleUser).put(updateUser).delete(deleteUser);
+
+router.route("/:userId/friends/:memberId").post(addMember).delete(deleteMember);
 
 module.exports = router;
