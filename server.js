8eb4222107e797fe12de86663386d6b53fb71064
Cory Thompson <cory.thompson98@gmail.com>
2022-04-15 10:51:34 -0500

    auth.js added to utils directory and schemas directory added with index, resolvers, and typeDefs

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const {ApolloServer} = require('apollo-server-express');
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
