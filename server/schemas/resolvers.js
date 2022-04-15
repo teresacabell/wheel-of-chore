c559f69f482f2eb39a305ba18cba5ba4908a71e3
teresacabell <teresacabll@gmail.com>
2022-04-14 21:01:36 -0500

    started on schemas

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,15 @@
+const { AuthenticationError } = require('apollo-server-express');
+const { signToken } = require('../utils/auth');
+const { User } = require('../models');
+
+const resolvers = {
+    Query: {
+        me: async (parent, args, context) => {
+            if (context.user) {
+                const userData = await User.findOne({ _id: context.user._id}).select('__v -password');
+                return userData
+            }
+            throw new AuthenticationError('You are not logged in');
+        },
+    },
+}
